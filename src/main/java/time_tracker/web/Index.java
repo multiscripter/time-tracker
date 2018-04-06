744f39e513ebe0cb5beb30ef1dfc45ce2fdf55c4
cyberbotx <ill-jah@yandex.ru>
2018-04-07 00:16:20 +0300

    Task Time tracker 0.1 1h

---
+++ src/main/java/time_tracker/web/Index.java
@@ -0,0 +1,9 @@
+package time_tracker.web;
+
+/**
+ * Класс TimeTracker контроллер приложения Трэкер времени.
+ *
+ * @author Goureev Ilya (mailto:ill-jah@yandex.ru)
+ * @version 2018-04-06
+ * @since 2018-04-06
+ */
---
+++ src/main/java/time_tracker/web/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for Time tracker app.
+*
+* @author Gureyev Ilya (mailto:ill-jah@yandex.ru)
+* @version 2018-04-06
+* @since 2018-04-06
+*/
+package time_tracker.web;
